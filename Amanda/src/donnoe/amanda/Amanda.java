7ccca305a004d6518fabb75c6e7fa060be03cc1e
Josh Donnoe <josh.donnoe@gmail.com>
2017-01-02 04:02:26 -0600

    re ordered members in both source and uml

--- Amanda/src/donnoe/amanda/Amanda.java
+++ Amanda/src/donnoe/amanda/Amanda.java
@@ -1,10 +1,7 @@
 package donnoe.amanda;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.*;
+import java.util.concurrent.*;
 import java.util.function.Function;
 
 /**
@@ -13,9 +10,25 @@ import java.util.function.Function;
  */
 public enum Amanda implements Function<String, String> {
     INSTANCE;
+
+    public final ExecutorService exec = Executors.newCachedThreadPool();
+
+    private static boolean isVerbose;
+
+    private PrintStream stream;
+
+    public static void main(String[] args) {
+        isVerbose = args.length > 1;
+        System.out.println(INSTANCE.apply(args[0]));
+        INSTANCE.exec.shutdown();
+    }
+
+    /**
+     * Set by entry point as either stderr or dev/null
+     */
     @Override
     public String apply(String t) {
-        
+
         //better to check the bool once rather than every time
         stream = isVerbose ? System.err : new PrintStream(new OutputStream() {
             @Override
@@ -24,32 +37,19 @@ public enum Amanda implements Function<String, String> {
         });
         return new ClassFile(t).toString();
     }
-    
-    public static void main(String[] args) {
-        isVerbose = args.length > 1;
-        System.out.println(INSTANCE.apply(args[0]));
-        INSTANCE.exec.shutdown();
-    }
-    
-    private static boolean isVerbose;
-    
+
     /**
-     * Set by entry point as either stderr or dev/null
-     */
-    private PrintStream stream;
-    
-    /**
-     * delegates to stream to possibly print object. This model allows me to create a message
-     * possibly with side-effects and still have the choice on whether it is displayed.
-     * Client code does not need to check is verbose flag was set. Although message does not need to be
-     * constructed if not in verbose mode but this way side effect still happens and client code
-     * does not know/care the mode 
-     * 
-     * @param o 
+     * delegates to stream to possibly print object. This model allows me to
+     * create a message possibly with side-effects and still have the choice on
+     * whether it is displayed. Client code does not need to check is verbose
+     * flag was set. Although message does not need to be constructed if not in
+     * verbose mode but this way side effect still happens and client code does
+     * not know/care the mode
+     *
+     * @param o
      */
     public void println(Object o) {
         stream.println(o);
     }
-    
-    public final ExecutorService exec = Executors.newCachedThreadPool();
+
 }
--- Amanda/src/donnoe/amanda/ClassFile.java
+++ Amanda/src/donnoe/amanda/ClassFile.java
@@ -1,28 +1,22 @@
 package donnoe.amanda;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOError;
-import java.io.IOException;
+import java.io.*;
 import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.concurrent.*;
+import java.util.stream.*;
+import static donnoe.amanda.Amanda.INSTANCE;
 /**
  *
  * @author joshuadonnoe
  */
 public class ClassFile extends Blob {
+
     public ClassFile(String fileName) {
         try (DataInputStream in = new DataInputStream(new FileInputStream(fileName))) {
-            Amanda.INSTANCE.
-            println(String.format("0x%X%n%3$s.%s", in.readInt(), in.readUnsignedShort(), in.readUnsignedShort()));
+            INSTANCE.println(String.format("0x%X%n%3$s.%s", in.readInt(), in.readUnsignedShort(), in.readUnsignedShort()));
             Map<Integer, BlockingQueue<Future<Constant>>> qs = IntStream.range(1, in.readUnsignedShort()).boxed().collect(Collectors.toMap(i -> i, i -> new ArrayBlockingQueue<>(1)));
 //            Map<Integer, Future<Constant>> CONSTANT_FUTURES = qs.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> Amanda.INSTANCE().exec.submit(() -> e.getValue().take().get())));
-        }
-        catch (IOException x) {
+        } catch (IOException x) {
             throw new IOError(x);
         }
     }
