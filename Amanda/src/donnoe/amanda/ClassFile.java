774e0c231470bd39e17fbcb1e0716f7be2317d05
Josh Donnoe <josh.donnoe@gmail.com>
2017-01-02 12:25:51 -0600

    altered classfile to have the innput stream as a instance variable and not a local so other objects can call methods on it
    created nifty template to implement all functinality of DIS without checking IOException at client. if IOE occurs, program must die so Error is thrown

--- Amanda/src/donnoe/amanda/ClassFile.java
+++ Amanda/src/donnoe/amanda/ClassFile.java
@@ -5,17 +5,37 @@ import java.util.Map;
 import java.util.concurrent.*;
 import java.util.stream.*;
 import static donnoe.amanda.Amanda.INSTANCE;
+import static donnoe.util.Functions.ExceptionalFunction;
 /**
  *
  * @author joshuadonnoe
  */
-public class ClassFile extends Blob {
+public final class ClassFile extends Blob {
 
     public ClassFile(String fileName) {
-        try (DataInputStream in = new DataInputStream(new FileInputStream(fileName))) {
-            INSTANCE.println(String.format("0x%X%n%3$s.%s", in.readInt(), in.readUnsignedShort(), in.readUnsignedShort()));
-            Map<Integer, BlockingQueue<Future<Constant>>> qs = IntStream.range(1, in.readUnsignedShort()).boxed().collect(Collectors.toMap(i -> i, i -> new ArrayBlockingQueue<>(1)));
-//            Map<Integer, Future<Constant>> CONSTANT_FUTURES = qs.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> Amanda.INSTANCE().exec.submit(() -> e.getValue().take().get())));
+        try {
+            in = new DataInputStream(new FileInputStream(fileName));
+        } catch (IOException x) {
+            throw new IOError(x);
+        }
+        INSTANCE.println(String.format("0x%X%n%3$s.%s", readInt(), readUnsignedShort(), readUnsignedShort()));
+        Map<Integer, BlockingQueue<Future<Constant>>> qs = IntStream.range(1, readUnsignedShort()).boxed().collect(Collectors.toMap(i -> i, i -> new ArrayBlockingQueue<>(1)));
+//        Map<Integer, Future<Constant>> CONSTANT_FUTURES = qs.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> Amanda.INSTANCE().exec.submit(() -> e.getValue().take().get())));
+    }
+
+    private DataInputStream in;
+    
+    public int readInt() {
+        return read(DataInputStream::readInt);
+    }
+    
+    public int readUnsignedShort() {
+        return read(DataInputStream::readUnsignedShort);
+    }
+    
+    private <T> T read(ExceptionalFunction<DataInputStream, T, IOException> f) {
+        try {
+            return f.apply(in);
         } catch (IOException x) {
             throw new IOError(x);
         }
---
+++ Util/src/donnoe/util/Functions.java
@@ -0,0 +1,12 @@
+package donnoe.util;
+
+/**
+ *
+ * @author joshuadonnoe
+ */
+public class Functions {
+    private Functions() {}
+    public interface ExceptionalFunction<T, R, X extends Throwable> {
+        R apply(T t) throws X;
+    }
+}
