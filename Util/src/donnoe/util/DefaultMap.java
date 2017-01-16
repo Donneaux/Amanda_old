fd18cc942b75c83fc3856187758bf9e1f7b8c845
Josh Donnoe <josh.donnoe@gmail.com>
2017-01-16 09:06:41 -0600

    especially useful if using double block

--- Util/src/donnoe/util/DefaultMap.java
+++ Util/src/donnoe/util/DefaultMap.java
@@ -1,9 +1,14 @@
 package donnoe.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public final class DefaultMap<K, V> extends LookupMap<K, V> {
 
+    public DefaultMap(V v) {
+        this(new HashMap<K, V>(), v);
+    }
+    
     public DefaultMap(Map<K, V> map, V v) {
         super(map, k -> v);
     }
