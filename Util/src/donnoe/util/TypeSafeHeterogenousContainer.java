256da270be1c2da18db74d800fce52669e4e0b75
Josh Donnoe <josh.donnoe@gmail.com>
2017-01-20 13:01:28 -0600

    created a container for Attributes

---
+++ Util/src/donnoe/util/TypeSafeHeterogenousContainer.java
@@ -0,0 +1,29 @@
+package donnoe.util;
+
+import java.util.Map;
+import static java.util.Collections.*;
+import java.util.List;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+/**
+ *
+ * @author joshuadonnoe
+ * @param <S>
+ */
+public class TypeSafeHeterogenousContainer<S> {
+    private final Map<Class<? extends S>, List<S>> map; 
+    
+    public TypeSafeHeterogenousContainer(Map<Class<? extends S>, List<S>> map) {
+        this.map = map;
+    }
+    
+    public final <S2 extends S> List<S> put(S2 value) {
+        Class<S2> clazz = (Class) value.getClass();
+        return map.merge(clazz, singletonList(value), (s1, s2) -> Stream.concat(s1.stream(), s2.stream()).collect(toList()));
+    }
+    
+    public final <S2 extends S> List<S2> get(Class<S2> clazz) {
+        List<S2> list = (List) map.get(clazz);
+        return list;
+    }
+}
